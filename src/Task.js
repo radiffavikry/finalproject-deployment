03a33ad44d4cc5b6f305ec29d5c44f36849a49ef
radiffavikry <ravizh702@gmail.com>
2022-12-16 00:30:55 +0700

    deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://radiffa-vikry-fe3000458.web.app" // TODO: replace this
+const studentName = "RADIFFA VIKRY RIZALULHAQ" // TODO: replace this
+const studentId = "FE3000458"
+
+export { url, studentName, studentId }
